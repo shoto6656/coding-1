3bf74dd297cf4740be7e8312ae5643fcea64a0c1
shoto6656 <jwilliams071@boyslatin.net>
2026-09-03 14:20:16 +0000

    the coding work for iankimble

---
+++ variableActivity.py
@@ -0,0 +1,22 @@
+# VARIABLES - a container that stores. you can
+# name your container almost anything you want, so long
+# as it is descriptive
+
+name= "jeremiah williams"
+yearsofexp= 2
+graduate= False
+
+# Variable Naming rules
+#1. A variable MUST start with letter and cannot start with a number
+# 2. A variable can be mixed of letters and numbers BUT not symbols
+# 3. A variable cannot be a python Keyword
+
+applecount= 56
+studentcount= 3
+optionaltravelinsurence=676767676767676767676767676767676767676767676767676767676767676
+bronzemembership= 1
+silvermembership=45
+goldmembership=89
+sprieprice= 90
+pepsiprice=790
+grapejuiceprice=890
