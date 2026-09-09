05cd4d07281630da36fbae26b62e3dd26992248b
shoto6656 <jwilliams071@boyslatin.net>
2026-09-09 14:36:40 +0000

    notes and work

---
+++ operators.py
@@ -0,0 +1,30 @@
+#assignment Operators- Exclusively used to
+#assign values yo variables
+#think key/value parings
+
+#we used a single equal sign to represent the assignment
+#operator
+name = 'Jeremiah'
+grade= 10
+school= True
+
+#Arthithmitic operators-used on numerical
+#data types to perform calculations
+#integers(whole numbers) and floats (decimal numbers)
+
+#print is a function that lets us show code
+#in the terminal
+print(3 + 3) # addition operator
+print(3.9 - 3) #subtraction
+print(12 / 3) # divsion
+print(3 * 3)  #multiplactaion
+
+#comparison operators - set of symbols used
+#to assess if data is the same or differnet and 
+# how they differ
+
+print( 10 > 1) # greater than operator
+print(10 < 2) # less than operator
+
+# 2 equal signs compare is something is the smae
+print ("jeremiah" == "jeremiah") #same as (false)
